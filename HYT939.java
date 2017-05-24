f1f766b181750ef2f44f10406bd207f04009dd9b
LoXeras <duff.dario@gmail.com>
2017-05-24 16:50:05 +0200

    Update HYT939.java

--- HYT939.java
+++ HYT939.java
@@ -4,9 +4,9 @@
 // This code is designed to work with the HYT939_I2CS I2C Mini Module available from ControlEverything.com.
 // https://www.controleverything.com/content/Humidity?sku=HYT939_I2CS#tabs-0-product_tabset-2
 
-//import com.pi4j.io.i2c.I2CBus;
-//import com.pi4j.io.i2c.I2CDevice;
-//import com.pi4j.io.i2c.I2CFactory;
+import com.pi4j.io.i2c.I2CBus;
+import com.pi4j.io.i2c.I2CDevice;
+import com.pi4j.io.i2c.I2CFactory;
 
 import java.net.*;
 import java.io.*;
@@ -17,28 +17,28 @@ public class HYT939
 	public static void main(String args[]) throws Exception
 	{
 		// Create I2CBus
-		//I2CBus bus = I2CFactory.getInstance(I2CBus.BUS_1);
-		// Get I2C device, HYT939 I2C address is 0x28(40)
-		//I2CDevice device = bus.getDevice(0x28);
+		I2CBus bus = I2CFactory.getInstance(I2CBus.BUS_1);
+		Get I2C device, HYT939 I2C address is 0x28(40)
+		I2CDevice device = bus.getDevice(0x28);
 
 		// Send normal mode command 
-		//device.write((byte)0x80);
-		//Thread.sleep(500);
+		device.write((byte)0x80);
+		Thread.sleep(500);
 
 		// Read 4 bytes of data
-		// humidity msb, humidity lsb, temp msb, temp lsb
-		//byte[] data = new byte[4];
-		//device.read(data,0,4);
+		humidity msb, humidity lsb, temp msb, temp lsb
+		byte[] data = new byte[4];
+		device.read(data,0,4);
 
 		// Convert the data to 14-bits
-		//double humidity = (((data[0] & 0x3F) * 256) + (data[1] & 0xFF)) * (100.0 / 16383.0);
-		//double cTemp = ((((data[2] & 0xFF) * 256) + (data[3] & 0xFC)) / 4) * (165.0 / 16383.0) - 40;
+		double humidity = (((data[0] & 0x3F) * 256) + (data[1] & 0xFF)) * (100.0 / 16383.0);
+		double cTemp = ((((data[2] & 0xFF) * 256) + (data[3] & 0xFC)) / 4) * (165.0 / 16383.0) - 40;
 		double cTemp = 15;
 		double humidity = 19;
 
 		// Output data to screen
-		//System.out.printf("Relative Humidity is : %.2f %%RH %n", humidity);
-		//System.out.printf("Temperature in Celsius is : %.2f C %n", cTemp);
+		System.out.printf("Relative Humidity is : %.2f %%RH %n", humidity);
+		System.out.printf("Temperature in Celsius is : %.2f C %n", cTemp);
 		//System.out.printf("Temperature in Fahrenheit is : %.2f F %n", fTemp);
 		
 		String url = "http://loxeras.com/api/insert.php";
