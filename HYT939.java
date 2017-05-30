c443f17cbab5374fd839a60cef13f4cf3afb615d
LoXeras <duff.dario@gmail.com>
2017-05-30 17:08:45 +0200

    Update HYT939.java

--- HYT939.java
+++ HYT939.java
@@ -47,8 +47,8 @@ public class HYT939
 		String charset = "UTF-8";  // Or in Java 7 and later, use the constant: java.nio.charset.StandardCharsets.UTF_8.name()
 		String s_humidity = Double.toString(humidity);
 		String s_temp = Double.toString(cTemp);
-		s_humidity = String.format("%2f", s_humidity);
-		s_temp = String.format("%2f", s_temp);
+		s_humidity = String.format("%.2f", s_humidity);
+		s_temp = String.format("%.2f", s_temp);
 
 		
 		
